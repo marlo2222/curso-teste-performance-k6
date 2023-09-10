f5d10ffa4e9358b651c0ed6c8b421a77cf072f54
marlo2222 <marlo_henrique@hotmail.com.br>
2023-09-10 15:06:50 -0300

    ajustes script aula 24

--- modulo5_conteudo_adicional/aula24.js
+++ modulo5_conteudo_adicional/aula24.js
@@ -1,29 +1,31 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import {sleep, check} from 'k6';
 
 export const options = {
-    vus: 1,
-    duration: '3s',
+    stages: [
+        { duration: '10s', target: 10 }, 
+        { duration: '10s', target: 10 },
+        { duration: '10s', target: 0 } 
+    ],
     thresholds: {
-        checks: ['rate > 0.99']
+        checks: ['rate > 0.95'],
+        http_req_duration: ['p(95) < 200']
     },
-    ext: {
-        loadimpact: {
-          projectID: 3624306,
-          name: "TESTE CURSO K6"
+    ext:{
+        loadimpact:{
+            projectID: 'Insira o seu project ID',
+            name: 'POC CURSO K6'
         }
-      }
+    }
 }
 
 export default function(){
-    const BASE_URL = 'https://test-api.k6.io/public/crocodiles/';
 
-    const res = http.get(BASE_URL);
-
-    check(res, {
-        'status code 200': (r) => r.status === 200
+    const BASE_URL = 'https://test-api.k6.io/public/crocodiles';
+    const res = http.get(BASE_URL)
+    
+    check(res,{
+        'status 200': (r) => r.status === 200
     });
+    sleep(1);
 }
-
-
-
